3ec292a220eb9699a9d6d3fe0e2761a7ff933fd1
jede <jede@oric.org>
2020-06-11 00:44:53 +0200

    Fix header

--- src/include/twilighte.h
+++ src/include/twilighte.h
@@ -5,4 +5,4 @@
 
 unsigned char twil_program_rambank(unsigned char bank, char *file, unsigned char set);
 unsigned char twil_clear_rambank(unsigned char bank, unsigned char set);
-unsigned char * display_signature_bank(unsigned char ROMRAM, unsigned char sector,  unsigned char bank);
+unsigned char * twil_display_signature_bank(unsigned char ROMRAM, unsigned char sector,  unsigned char bank);
