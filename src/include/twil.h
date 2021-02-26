9e76b0013a8cbb6b9878147f428b8f3d1dffcee7
jede <jede@oric.org>
2021-02-26 23:04:50 +0100

    add registers

--- src/include/twil.h
+++ src/include/twil.h
@@ -5,7 +5,11 @@
 
 #define TWIL_LIB_VERSION_2021_2 0
 
-unsigned char twil_program_rambank(unsigned char bank, char *file, unsigned char set);
+#define TWILIGHTE_REGISTER          0x342
+#define TWILIGHTE_BANKING_REGISTER  0x343
+
+unsigned char twil_program_rambank(unsigned char bank, char *file, unsigned char set); 
+//unsigned char twil_program_rambank_id(char *file, unsigned char rambankid); 
 unsigned char twil_clear_rambank(unsigned char bank, unsigned char set);
 unsigned char * twil_display_signature_bank(unsigned char sector,  unsigned char bank);
 unsigned char twil_lib_version(void);
