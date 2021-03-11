ed992c4214799282df1410372d4d90d721543422
jede <jede@oric.org>
2021-03-12 00:52:17 +0100

    Add _twil_get_registers_from_id_bank

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 [![build](https://github.com/orix-software/twillib/actions/workflows/main.yml/badge.svg?branch=master)](https://github.com/orix-software/twillib/actions/workflows/main.yml)
 
 Lib for twilighte board
--- src/include/twil.h
+++ src/include/twil.h
@@ -6,11 +6,17 @@
 #define TWILIGHTE_REGISTER          0x342
 #define TWILIGHTE_BANKING_REGISTER  0x343
 
-unsigned char twil_program_rambank(unsigned char bank, char *file, unsigned char set); 
-//unsigned char twil_program_rambank_id(char *file, unsigned char rambankid); 
-unsigned char twil_clear_rambank(unsigned char bank, unsigned char set);
-//unsigned char * twil_display_signature_bank(unsigned char sector,  unsigned char bank);
 unsigned char twil_lib_version(void);
+
+unsigned char twil_program_rambank(unsigned char bank, char *file, unsigned char set); 
+/*Clear a ram bank*/
+unsigned char twil_clear_rambank(unsigned char bank, unsigned char set);
+
+
 unsigned char twil_get_id_bank(unsigned char bank, unsigned char set);
+/*Set default bank signature*/
 void twil_set_bank_signature(char *signature);
+
+unsigned char twil_get_registers_from_id_bank(unsigned char bank);
+
 
